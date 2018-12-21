358efea76ba761c0dcbf22ba38223c498ea23668
julien marzin <julien.marzin@outlook.com>
2018-12-21 23:33:14 +0100

    init & first test

---
+++ ChristmasTreeKata/ChristmasTree.Tests/ChristmasTreeTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree.Tests
+{
+    [TestFixture]
+    public class ChristmasTreeTests
+    {
+        [Test]
+        public void should_generate_tree_1()
+        {
+            int input = 1;
+            string expected = "*";
+            string output = ChristmasTreeKata.Program.GenerateChristmasTree(input);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void should_generate_tree_2()
+        {
+            int input = 2;
+
+            string expected = string.Empty;
+            expected += " *";
+            expected += "***";
+
+            string output = ChristmasTreeKata.Program.GenerateChristmasTree(input);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
---
+++ ChristmasTreeKata/ChristmasTreeKata/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTreeKata
+{
+    public static class Program
+    {
+        private const string LEAF = "*";
+
+        static void Main(string[] args)
+        {
+        }
+
+        public static string GenerateChristmasTree(int input)
+        {
+            return LEAF;
+        }
+    }
+}
